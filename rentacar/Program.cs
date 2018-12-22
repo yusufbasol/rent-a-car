b8938e9c5322ecfeea033b17a598dfb881f94360
yusufbasol <basolyusuff@gmail.com>
2018-12-22 22:44:38 +0300

    Update Program.cs

--- rentacar/Program.cs
+++ rentacar/Program.cs
@@ -15,7 +15,7 @@ namespace rentacar
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Admin());
         }
     }
 }
